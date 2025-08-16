1909ffc62109d96678315948fdb27a54a705a2f9
Birukgezahegnalemu <gezahegnalemu2020@gmail.com>
2025-08-16 07:54:59 +0300

    welcome to git

---
+++ tranningfromschool.py
@@ -0,0 +1,6 @@
+(print) welcome to this project
+(print) we will show you new things
+(print) like how to make phthon projects ok
+(print) time to start :-phthon is a widly used high-level programming language for general purpose of programming.here are code of it
+(print) x=10 y=23 z=(x*y)
+(print) thank you for coming
